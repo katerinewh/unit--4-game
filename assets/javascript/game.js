686c8822324d0644223fc8de5ebbbdc70f48a0bf
Katerine <katerinehw@att.net>
2018-11-12 12:08:56 -0800

    Monday mid

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,37 @@
 //game options//
-var targetNumber = "";
-$("#number-to-guess").text(targetNumber);
-var wins= 0;
-var losses=0;
+var targetNumber = Math.floor(Math.random() * 120 + 1);
+console.log(targetNumber)
+$("#number_to_guess").text(targetNumber);
+var wins = 0;
+var losses = 0;
+
 
 
 //give value to the crystals
-var numberOptions = [10, 6, 3, 9];
-var counter=0;
-//create for loop
-for (var i = 0; i < numberOptions.length; i++) {
 
-//     // For each iteration, we will create an imageCrystal
-//     var imageCrystal = $("<img>");
+var counter = 0;
+var hashnu = Math.floor(Math.random() * 20) + 1;
+var pastel = Math.floor(Math.random() * 20) + 1;
+var quartz = Math.floor(Math.random() * 20) + 1;
+var varied = Math.floor(Math.random() * 20) + 1;
 
-//     // First each crystal will be given the class ".crystal-image".
-//     // This will allow the CSS to take effect.
-//     imageCrystal.addClass("crystal-image");
+$("#hashnu").attr("data-value", hashnu)
+$("#pastel").attr("data-value", pastel)
+$("#quartz").attr("data-value", quartz)
+$("#varied").attr("data-value", varied)
 
-//     // Each imageCrystal will be given a src link to the crystal image
-//     imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+function reset() {
+    computerGuess = [Math.floor(Math.random())];
+    console.log(computerGuess);
+}
+// //  hardcoded HTML elements
+// $(".center").on("click", function(event){
 
-//     // Each imageCrystal will be given a data attribute called data-crystalValue.
-//     // This data attribute will be set equal to the array value.
-//     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-//     // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-//     $("#crystals").append(imageCrystal);
-// }
-//     }
-
-// // This time, our click event applies to every single crystal on the page. Not just one.
-// $(".crystal-image").on("click", function () {
-
-//     // Determining the crystal's value requires us to extract the value from the data attribute.
-//     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-//     // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-//     // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
-
+// })
+$(".center").click(function (event) {
+    var userclickvalue = $(this).data("value");
+    counter = counter + userclickvalue;
+    console.log(counter)
     // scoring
     alert("New score: " + counter);
 
@@ -50,13 +39,61 @@ for (var i = 0; i < numberOptions.length; i++) {
         alert("You win!");
         // update wins
         wins++;
-        
+        reset();
+
     }
 
     else if (counter >= targetNumber) {
         alert("You lose!!");
         // update losses
-        losses ++;
+        losses++;
+        reset();
     }
-
 });
+
+
+
+
+// dynamically added (JS) elements
+// $(document).on("click", ".center", function(){
+
+// })
+
+// //create for loop
+// for (var i = 0; i < numberOptions.length; i++) {
+
+// //     // For each iteration, we will create an imageCrystal
+// //     var imageCrystal = $("<img>");
+
+// //     // First each crystal will be given the class ".crystal-image".
+// //     // This will allow the CSS to take effect.
+// //     imageCrystal.addClass("crystal-image");
+
+// //     // Each imageCrystal will be given a src link to the crystal image
+// //     imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+// //     // Each imageCrystal will be given a data attribute called data-crystalValue.
+// //     // This data attribute will be set equal to the array value.
+// //     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+// //     // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+// //     $("#crystals").append(imageCrystal);
+// // }
+// //     }
+
+// // // This time, our click event applies to every single crystal on the page. Not just one.
+// // $(".crystal-image").on("click", function () {
+
+// //     // Determining the crystal's value requires us to extract the value from the data attribute.
+// //     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+// //     // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+// //     // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+
+//     var crystalValue = ($(this).attr("data-crystalvalue"));
+//     crystalValue = parseInt(crystalValue);
+//     // Every click, from every crystal adds to the global counter.
+//     counter += crystalValue;
+
+
+    // };
+
